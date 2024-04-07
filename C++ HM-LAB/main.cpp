d8fcc533b0be5e3701d8547af78d71000b87bbf7
vadimday <asdzxc123fg@mail.ru>
2024-04-07 19:26:39 +0300

    lab-done

--- C++ HM-LAB/main.cpp
+++ C++ HM-LAB/main.cpp
@@ -1,255 +1,141 @@
 #include <iostream>
-
 #include <string>
 
-#include <algorithm>
-
-struct Book {
-
-    std::string title;
-
-    std::string author;
-
-    std::string publisher;
-
-    std::string genre;
-
+struct WashingMachine {
+    std::string brand;
+    std::string color;
+    double width;
+    double length;
+    double height;
+    double power; 
+    int spinSpeed; 
+    int heatingTemperature; 
 };
 
-void printBook(const Book& book) {
+int main() {
+    WashingMachine myWashingMachine;
 
-    std::cout << "name: " << book.title << std::endl;
+    myWashingMachine.brand = "ExampleBrand";
+    myWashingMachine.color = "White";
+    myWashingMachine.width = 60.0; 
+    myWashingMachine.length = 60.0; 
+    myWashingMachine.height = 85.0; 
+    myWashingMachine.power = 1500.0; 
+    myWashingMachine.spinSpeed = 1200; 
+    myWashingMachine.heatingTemperature = 40; 
 
-    std::cout << "author: " << book.author << std::endl;
+    std::cout << "Brand: " << myWashingMachine.brand << std::endl;
+    std::cout << "Color: " << myWashingMachine.color << std::endl;
+    std::cout << "Width: " << myWashingMachine.width << " cm" << std::endl;
+    std::cout << "Length: " << myWashingMachine.length << " cm" << std::endl;
+    std::cout << "Height: " << myWashingMachine.height << " cm" << std::endl;
+    std::cout << "Power: " << myWashingMachine.power << " watts" << std::endl;
+    std::cout << "Spin Speed: " << myWashingMachine.spinSpeed << " RPM" << std::endl;
+    std::cout << "Heating Temperature: " << myWashingMachine.heatingTemperature << " Celsius" << std::endl;
 
-    std::cout << "publishing house: " << book.publisher << std::endl;
-
-    std::cout << "genre: " << book.genre << std::endl;
-
-    std::cout << std::endl;
-
+    return 0;
 }
 
-void editBook(Book& book) {
 
-    std::cout << "enter new book: ";
 
-    std::getline(std::cin >> std::ws, book.title);
+#include <iostream>
+#include <string>
 
-    std::cout << "enter new author: ";
+struct Iron {
+    std::string brand;
+    std::string model;
+    std::string color;
+    int minTemperature; 
+    int maxTemperature; 
+    bool hasSteam; 
+    double power; 
+};
 
-    std::getline(std::cin >> std::ws, book.author);
+int main() {
+    
+    Iron myIron;
 
-    std::cout << "enter new publishing house: ";
+    
+    myIron.brand = "ExampleBrand";
+    myIron.model = "ExampleModel";
+    myIron.color = "Black";
+    myIron.minTemperature = 100; 
+    myIron.maxTemperature = 200; 
+    myIron.hasSteam = true;
+    myIron.power = 1500.0; 
+    std::cout << "Brand: " << myIron.brand << std::endl;
+    std::cout << "Model: " << myIron.model << std::endl;
+    std::cout << "Color: " << myIron.color << std::endl;
+    std::cout << "Minimum Temperature: " << myIron.minTemperature << " Celsius" << std::endl;
+    std::cout << "Maximum Temperature: " << myIron.maxTemperature << " Celsius" << std::endl;
+    std::cout << "Steam Supply: " << (myIron.hasSteam ? "Yes" : "No") << std::endl;
+    std::cout << "Power: " << myIron.power << " watts" << std::endl;
 
-    std::getline(std::cin >> std::ws, book.publisher);
-
-    std::cout << "enter new genre: ";
-
-    std::getline(std::cin >> std::ws, book.genre);
-
+    return 0;
 }
 
-void printAllBooks(const Book library[], int size) {
 
-    std::cout << "library:" << std::endl;
 
-    for (int i = 0; i < size; i++) {
+#include <iostream>
+#include <string>
 
-        std::cout << "boook " << i + 1 << ":" << std::endl;
+struct Boiler {
+    std::string brand;
+    std::string color;
+    double power; 
+    double volume; 
+    int heatingTemperature; 
+};
 
-        printBook(library[i]);
+int main() {
+ 
+    Boiler myBoiler;
+    myBoiler.brand = "ExampleBrand";
+    myBoiler.color = "Silver";
+    myBoiler.power = 2000.0; 
+    myBoiler.volume = 1.5; 
+    myBoiler.heatingTemperature = 100;
+    std::cout << "Brand: " << myBoiler.brand << std::endl;
+    std::cout << "Color: " << myBoiler.color << std::endl;
+    std::cout << "Power: " << myBoiler.power << " watts" << std::endl;
+    std::cout << "Volume: " << myBoiler.volume << " liters" << std::endl;
+    std::cout << "Heating Temperature: " << myBoiler.heatingTemperature << " Celsius" << std::endl;
 
-    }
-
+    return 0;
 }
 
-void searchByAuthor(const Book library[], int size, const std::string& author) {
 
-    std::cout << "books author " << author << ":" << std::endl;
+#include <iostream>
+#include <string>
 
-    bool found = false;
+struct Animal {
+    std::string name;
+    std::string animalClass;
+    std::string nickname;
+};
 
-    for (int i = 0; i < size; i++) {
-
-        if (library[i].author == author) {
-
-            printBook(library[i]);
-
-            found = true;
-
-        }
-
-    }
-
-    if (!found) {
-
-        std::cout << "error." << std::endl;
-
-    }
-
+void fillAnimal(Animal& animal, const std::string& name, const std::string& animalClass, const std::string& nickname) {
+    animal.name = name;
+    animal.animalClass = animalClass;
+    animal.nickname = nickname;
 }
 
-void searchByTitle(const Book library[], int size, const std::string& title) {
-
-    std::cout << "book with name \"" << title << "\":" << std::endl;
-
-    bool found = false;
-
-    for (int i = 0; i < size; i++) {
-
-        if (library[i].title == title) {
-
-            printBook(library[i]);
-
-            found = true;
-
-        }
-
-    }
-
-    if (!found) {
-
-        std::cout << "error." << std::endl;
-
-    }
-
+void displayAnimal(const Animal& animal) {
+    std::cout << "Name: " << animal.name << std::endl;
+    std::cout << "Class: " << animal.animalClass << std::endl;
+    std::cout << "Nickname: " << animal.nickname << std::endl;
 }
-
-bool compareByTitle(const Book& book1, const Book& book2) {
-
-    return book1.title < book2.title;
-
-}
-
-bool compareByAuthor(const Book& book1, const Book& book2) {
-
-    return book1.author < book2.author;
-
-}
-
-bool compareByPublisher(const Book& book1, const Book& book2) {
-
-    return book1.publisher < book2.publisher;
-
-}
-
-void sortBooksByTitle(Book library[], int size) {
-
-    std::sort(library, library + size, compareByTitle);
-
-}
-
-void sortBooksByAuthor(Book library[], int size) {
-
-    std::sort(library, library + size, compareByAuthor);
-
-}
-
-void sortBooksByPublisher(Book library[], int size) {
-
-    std::sort(library, library + size, compareByPublisher);
-
-}
-
-void removeBookByCriteria(Book library[], int& size, const std::string& criteria) {
-
-    int removedCount = 0;
-
-    for (int i = 0; i < size; i++) {
-
-        if (library[i].title == criteria || library[i].author == criteria ||
-
-            library[i].publisher == criteria || library[i].genre == criteria) {
-
-            removedCount++;
-
-        }
-        else {
-
-            library[i - removedCount] = library[i];
-
-        }
-
-    }
-
-    size -= removedCount;
-
-    std::cout << "delete " << removedCount << " choose." << std::endl;
-
-}
-
-void addNewBook(Book library[], int& size) {
-
-    if (size >= 10) {
-
-        std::cout << "full." << std::endl;
-
-        return;
-
-    }
-
-    Book newBook;
-
-    std::cout << "enter new book: ";
-
-    std::getline(std::cin >> std::ws, newBook.title);
-
-    std::cout << "enter new author: ";
-
-    std::getline(std::cin >> std::ws, newBook.author);
-
-    std::cout << "enter new publishing house: ";
-
-    std::getline(std::cin >> std::ws, newBook.publisher);
-
-    std::cout << "enter new genre: ";
-
-    std::getline(std::cin >> std::ws, newBook.genre);
-
-    library[size] = newBook;
-
-    size++;
-
-    std::cout << "good." << std::endl;
-
+void voteForAnimal(Animal& animal) {
+    std::cout << "You voted for " << animal.name << "!" << std::endl;
 }
 
 int main() {
+  
+    Animal myAnimal;
+    fillAnimal(myAnimal, "Tiger", "Mammal", "Stripes");
 
-    Book library[10];
-
-    int size = 0;
-
-
-    Book book1 = { "book 1", "author 1", "pub name 1", "genre 1" };
-
-    Book book2 = { "book 2", "author 2", "pub name 2", "genre 2" };
-
-    library[size++] = book1;
-
-    library[size++] = book2;
-    addNewBook(library, size);
-
-    editBook(library[0]);
-
-    printAllBooks(library, size);
-
-    searchByAuthor(library, size, "author 1");
-
-    searchByTitle(library, size, "book 2");
-
-    sortBooksByTitle(library, size);
-
-    sortBooksByAuthor(library, size);
-
-    sortBooksByPublisher(library, size);
-
-    removeBookByCriteria(library, size, "genre 2");
-
-    printAllBooks(library, size);
+    displayAnimal(myAnimal);
+    voteForAnimal(myAnimal);
 
     return 0;
-
 }
-
